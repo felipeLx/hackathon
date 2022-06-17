6255469cdf436a1b1fba19035260f0349c8ab6ef
felipeLx <felipealvesfinanceiro@gmail.com>
2022-06-17 19:14:18 -0300

    test on prediction

--- pages/1_🔬_Metastase_Detector.py
+++ pages/1_🔬_Metastase_Detector.py
@@ -30,10 +30,10 @@ if uploaded_file is not None:
         input_arr = tf.keras.preprocessing.image.img_to_array(file_bytes)
         input_arr = np.array([input_arr])
         probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
-        probability_model.compile(optimizer="rmsprop",
+        new_model = probability_model.compile(optimizer="rmsprop",
         loss="sparse_categorical_crossentropy",
         metrics=["sparse_categorical_accuracy"],)
-        prediction = probability_model.evaluate(input_arr)
+        prediction = new_model.evaluate(input_arr)
         c.write(prediction)
         dict_pred = {0: 'Benigno/Normal', 1: 'Maligno'}
         result = dict_pred[np.argmax(prediction)]
