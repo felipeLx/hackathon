d017b26f8377f0bb74a2fbe5b5f6303e76dbc0d7
felipeLx <felipealvesfinanceiro@gmail.com>
2022-06-17 15:44:43 -0300

    add dataframe to variable page

--- pages/2_🧬_Variaveis_Detector.py
+++ pages/2_🧬_Variaveis_Detector.py
@@ -23,9 +23,6 @@ df = df.iloc[:,0:32]
 # df['diagnisis_int'] = np.where(df['diagnosis'] == 'M', 1, 0)
 # print(df.info())
 
-#cabeçalho
-st.subheader('Informações dos dados')
-
 #dados de entrada
 X= df.iloc[:, 2:32]
 Y= df['diagnosis']
@@ -117,14 +114,17 @@ dtc.fit(X_train, y_train)
 
 # result_report = classification_report(user_input_variables, y_test, target_names=target_name)
 # print(result_report)
+user_input_variables= get_user_data(radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean, concavity_mean, concave_points_mean, symmetry_mean, fractal_dimension_mean, radius_se, texture_se, perimeter_se, area_se, smoothness_se, compactness_se, concavity_se, concave_points_se, symmetry_se, fractal_dimension_se, radius_worst, texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst, concavity_worst, concave_points_worst, symmetry_worst, fractal_dimension_worst)
 
 
 c = st.container()
+
+c.subheader('Informações dos dados')
+c.dataframe(user_input_variables)
+
 c.subheader('Previsão: ')
 generate_pred = c.button("Gerar Previsão")    
 if generate_pred:
-    user_input_variables= get_user_data(radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean, concavity_mean, concave_points_mean, symmetry_mean, fractal_dimension_mean, radius_se, texture_se, perimeter_se, area_se, smoothness_se, compactness_se, concavity_se, concave_points_se, symmetry_se, fractal_dimension_se, radius_worst, texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst, concavity_worst, concave_points_worst, symmetry_worst, fractal_dimension_worst)
-
     #Previsao
     prediction = dtc.predict(user_input_variables)
     # target_name = ['B', 'M']
