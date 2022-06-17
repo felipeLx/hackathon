b06e961d6ad9dbacfa79cac9134913ffebf90e89
felipeLx <felipealvesfinanceiro@gmail.com>
2022-06-17 17:55:08 -0300

    fix probabilty image pages

--- pages/1_🔬_Metastase_Detector.py
+++ pages/1_🔬_Metastase_Detector.py
@@ -29,9 +29,8 @@ if uploaded_file is not None:
         model = loadMetModel()
         input_arr = tf.keras.preprocessing.image.img_to_array(file_bytes)
         input_arr = np.array([input_arr])
-        # probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
-        # prediction = probability_model.predict(input_arr)
-        prediction = tf.keras.Sequential.predict(input_arr)
+        probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+        prediction = probability_model.predict(input_arr, force=True)
         dict_pred = {0: 'Benigno/Normal', 1: 'Maligno'}
         result = dict_pred[np.argmax(prediction)]
         value = 0
