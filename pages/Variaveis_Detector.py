6fd58861d0f75d7bf9abd918453779ee577b4b8b
felipeLx <felipealvesfinanceiro@gmail.com>
2022-06-16 22:58:19 -0300

    keras issue

--- pages/Variaveis_Detector.py
+++ pages/Variaveis_Detector.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
 #Titulo
-st.write("""
+st.title("""
 Prevendo Câncer\n
 App que utiliza machine learn para prever possível Câncer de mama
 Fonte: 
