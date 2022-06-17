f08eef756ee3ba81b9b94722656c318c77b4a86e
felipeLx <felipealvesfinanceiro@gmail.com>
2022-06-17 00:28:03 -0300

    fix variable page

--- pages/Variaveis_Detector.py
+++ pages/Variaveis_Detector.py
@@ -5,6 +5,7 @@ import streamlit as st
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 #Titulo
 st.title("""
@@ -15,6 +16,10 @@ Fonte:
 
 #dataset
 df = pd.read_csv("pages/models/data.csv")
+#print(df.isna().sum())
+#print(df.info())
+df = df.iloc[:,0:32]
+print(df.info())
 
 #cabeçalho
 st.subheader('Informações dos dados')
@@ -22,7 +27,7 @@ st.subheader('Informações dos dados')
 
 #dados de entrada
 
-X= df.drop(['diagnosis'], axis=1)
+X= df.iloc[:, 2:32]
 Y= df['diagnosis']
 
 #Dados em treinamentos e teste
@@ -34,25 +39,64 @@ def get_user_data():
     texture_mean = st.sidebar.slider('Textura Média', 1.120, 21.604, 17.914)
     perimeter_mean = st.sidebar.slider('Perímetro Médio', 2.000, 115.365, 78.075)
     area_mean = st.sidebar.slider('Área Média', 21.135, 978.376, 462.790)
-    smoothness = st.sidebar.slider('Suavidade', 0.006, 0.103, 0.092)
-    compactness = st.sidebar.slider('Compacidade', 0.0214, 0.145, 0.080)
-    concavity = st.sidebar.slider('Concavidade', 0.0259, 0.161, 0.046)
-    concave_points = st.sidebar.slider('Pontos Côncavos', 0.009, 0.88, 0.025)
-    symmetry = st.sidebar.slider('Simetria', 0.020, 0.193, 0.174)
-    fractal_dimension = st.sidebar.slider('Dimensão Fractual', 0.003, 0.063, 0.060)
+    smoothness_mean = st.sidebar.slider('Suavidade Média', 0.006, 0.103, 0.092)
+    compactness_mean = st.sidebar.slider('Compacidade Média', 0.0214, 0.145, 0.080)
+    concavity_mean = st.sidebar.slider('Concavidade Média', 0.0259, 0.161, 0.046)
+    concave_points_mean = st.sidebar.slider('Pontos Côncavos Média', 0.009, 0.88, 0.025)
+    symmetry_mean = st.sidebar.slider('Simetria Média', 0.020, 0.193, 0.174)
+    fractal_dimension_mean = st.sidebar.slider('Dimensão Fractual Média', 0.003, 0.063, 0.060)
+    radius_se = st.sidebar.slider('Raio SE',  0.284, 17.463, 12.146)
+    texture_se = st.sidebar.slider('Textura SE', 1.120, 21.604, 17.914)
+    perimeter_se = st.sidebar.slider('Perímetro SE', 2.000, 115.365, 78.075)
+    area_se = st.sidebar.slider('Área SE', 21.135, 978.376, 462.790)
+    smoothness_se = st.sidebar.slider('Suavidade SE', 0.006, 0.103, 0.092)
+    compactness_se = st.sidebar.slider('Compacidade SE', 0.0214, 0.145, 0.080)
+    concavity_se = st.sidebar.slider('Concavidade SE', 0.0259, 0.161, 0.046)
+    concave_points_se = st.sidebar.slider('Pontos Côncavos SE', 0.009, 0.88, 0.025)
+    symmetry_se = st.sidebar.slider('Simetria SE', 0.020, 0.193, 0.174)
+    fractal_dimension_se = st.sidebar.slider('Dimensão Fractual SE', 0.003, 0.063, 0.060)
+    radius_worst = st.sidebar.slider('Raio Pior',  0.284, 17.463, 12.146)
+    texture_worst = st.sidebar.slider('Textura Pior', 1.120, 21.604, 17.914)
+    perimeter_worst = st.sidebar.slider('Perímetro Pior', 2.000, 115.365, 78.075)
+    area_worst = st.sidebar.slider('Área Pior',21.135, 978.376, 462.790)
+    smoothness_worst = st.sidebar.slider('Suavidade Pior', 0.006, 0.103, 0.092)
+    compactness_worst = st.sidebar.slider('Compacidade Pior', 0.0214, 0.145, 0.080)
+    concavity_worst = st.sidebar.slider('Concavidade Pior', 0.0259, 0.161, 0.046)
+    concave_points_worst = st.sidebar.slider('Pontos Côncavos Pior', 0.009, 0.88, 0.025)
+    symmetry_worst =  st.sidebar.slider('Simetria Pior', 0.020, 0.193, 0.174)
+    fractal_dimension_worst = st.sidebar.slider('Dimensão Fractual Pior', 0.003, 0.063, 0.060)
 
     user_data={
             'Raio Médio' : radius_mean,
             'Textura Média' : texture_mean,
             'Perímetro Médio' : perimeter_mean,
             'Área Média' : area_mean,
-            'Suavidade' : smoothness,
-            'Compacidade' : compactness,
-            'Concavidade' : concavity,
-            'Concavidade' : concave_points,
-            'Pontos Côncavos' : concave_points,
-            'Simetria' : symmetry,
-            'Dimensão Fractual' : fractal_dimension
+            'Suavidade Média' : smoothness_mean,
+            'Compacidade Média' : compactness_mean,
+            'Concavidade Média' : concavity_mean,
+            'Pontos Concavidade Média' : concave_points_mean,
+            'Simetria Média' : symmetry_mean,
+            'Dimensão Fractual Média' : fractal_dimension_mean,
+            'Raio SE' : radius_se,
+            'Textura SE' : texture_se,
+            'Perímetro SE' : perimeter_se,
+            'Área SE' : area_se,
+            'Suavidade SE' : smoothness_se,
+            'Compacidade SE' : compactness_se,
+            'Concavidade SE' : concavity_se,
+            'Pontos Côncavos SE' : concave_points_se,
+            'Simetria SE' : symmetry_se,
+            'Dimensão Fractual SE' : fractal_dimension_se,
+            'Raio Pior' : radius_worst,
+            'Textura Pior' : texture_worst,
+            'Perímetro Pior' : perimeter_worst,
+            'Área Pior' : area_worst,
+            'Suavidade Pior' : smoothness_worst,
+            'Compacidade Pior' : compactness_worst,
+            'Concavidade Pior' : concavity_worst,
+            'Pontos Côncavos Pior' : concave_points_worst,
+            'Simetria Pior' : symmetry_worst,
+            'Dimensão Fractual Pior' : fractal_dimension_worst
             }
 
     features= pd.DataFrame(user_data, index=[0])
@@ -60,11 +104,11 @@ def get_user_data():
 user_input_variables= get_user_data()
 
 #Grafico
-graf = st.bar_chart(user_input_variables)
-st.subheader('Dados do usuário')
-st.write(user_input_variables)
+#graf = st.bar_chart(user_input_variables)
+#st.subheader('Dados do usuário')
+#st.write(user_input_variables)
 
-dtc = DecisionTreeClassifier(criterion='entropy', max_depth=3)
+dtc = RandomForestClassifier(n_estimators = 10, criterion='entropy', random_state=0)
 dtc.fit(X_train, y_train)
 
 #Acuracia
@@ -73,5 +117,7 @@ st.write(accuracy_score(y_test, dtc.predict(x_test)) * 100)
 #Previsao
 prediction = dtc.predict(user_input_variables)
 print(prediction)
-st.subheader('Previsão: ')
-st.write(prediction)
+
+c = st.container()
+c.subheader('Previsão: ')
+c.metric(prediction)
